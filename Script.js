e4d6f0bb417c5c9d02126cee6bc806b5e2b6d23f
adeslim2006 <salimadebola@gmail.com>
2023-06-15 22:44:51 +0100

    pause solution creation till further notice

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 const content = ["rock", "paper", "scissors"];
-let playerSelection = prompt("Rock, Paper, Scissors?!").toLocaleLowerCase();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
    
@@ -8,40 +8,61 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
    let ans = "";
-      
-   if (playerSelection === content[1] && computerSelection === content[0]) {
+   
+   if (playerSelection === content[1] && computerSelection === content[0]) { 
 
-      ans = `You Win! ${playerSelection} beats ${computerSelection}`
+      ans = `You Win! ${playerSelection} wraps ${computerSelection}`
 
    } else if (playerSelection === content[0] && computerSelection === content[2]) {
 
-      ans = `You Win! ${playerSelection} beats ${computerSelection}`
+      ans = `You Win! ${playerSelection} breaks ${computerSelection}`
 
    } else if (playerSelection === content[2] && computerSelection === content[1]) {
-      
-      ans = `You Win! ${playerSelection} beats ${computerSelection}`
+
+      ans = `You Win! ${playerSelection} cuts ${computerSelection}`
 
    } else if (playerSelection === content[0] && computerSelection === content[1]) {
 
-      ans = `You Lose! ${computerSelection} beats ${playerSelection}`
+      ans = `You Lose! ${computerSelection} wraps ${playerSelection}`
 
    } else if (playerSelection === content[2] && computerSelection === content[0]) {
       
-      ans = `You Lose! ${computerSelection} beats ${playerSelection}`
+      ans = `You Lose! ${computerSelection} breaks ${playerSelection}`
 
    } else if (playerSelection === content[1] && computerSelection === content[2]) {
 
-      ans = `You Lose! ${computerSelection} beats ${playerSelection}`
+      ans = `You Lose! ${computerSelection} cuts ${playerSelection}`
 
    } else {
-      ans = `Tie! Play Again?`
+      ans = `A Tie! Play Again?`
    }
-   
-
 return ans;
+
 };
 
+function Game() {
+   
 
+    return console.log(playRound(playerSelection, computerSelection));
+   
+}
 
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+const playerScore = 0;
+const computerScore = 0;
+
+// Score tracker
+function Scorecount(playerScore, computerScore) {
+   
+   if (playerScore == 5 || computerScore == 5) {
+       if (playerScore > computerScore) {
+           console.log("You Win!")
+           
+       } else if (playerScore < computerScore) {
+         console.log("You Lose!")
+   } else {
+      console.log("It's a tie")
+   }
+}
+}
+
+Game();
